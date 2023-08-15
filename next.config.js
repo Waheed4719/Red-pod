889693f8611cb1e984f1d605466ef2e348910a5f
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-08-15 08:12:44 +0600

    Fix mp3 file issues

--- next.config.js
+++ next.config.js
@@ -1,25 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack: (config) => {
-        config.module.rules.push({
-          test: /\.(mp3)$/,
-          use: [
-            {
-              loader: 'file-loader', // Use the file-loader to process MP3 files
-              options: {
-                name: '[name].[ext]', // Keep the original file name and extension
-                publicPath: '/assets/music/', // Change this path as needed
-                outputPath: 'static/sounds/', // Change this path as needed
-              },
-            },
-          ],
-        });
-    
-        return config;
-      },
-      images: {
-        domains: ['upload.wikimedia.org'], // Add your allowed domains here
-      },
-}
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(ogg|mp3|wav|mpe?g)$/i,
+      use: [
+        {
+          loader: "url-loader",
+          options: {
+            name: "[name]-[hash].[ext]",
+          },
+        },
+      ],
+    });
+    return config;
+  },
+  images: {
+    domains: ["upload.wikimedia.org"], // Add your allowed domains here
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
